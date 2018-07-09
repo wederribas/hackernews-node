759dbb37f7df627bf0efd078dbff1aa9999ab818
wederribas <wederribas@gmail.com>
2018-07-09 17:24:29 -0300

    Add new query fields and its resolvers to make CRUD and move schema out

--- src/index.js
+++ src/index.js
@@ -8,34 +8,56 @@ let links = [
   }
 ];
 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`;
+let idCount = links.length;
 
 const resolvers = {
   Query: {
     info: () => null,
-    feed: () => links
+    feed: () => links,
+    link: (root, args) => {
+      const foundLink = links.filter((link, index) => {
+        return link.id === args.id;
+      });
+      return foundLink[0];
+    }
   },
 
-  Link: {
-    id: root => root.id,
-    description: root => root.description,
-    url: root => root.url
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    },
+
+    updateLink: (root, args) => {
+      const updatedLink = links.filter((link, index) => {
+        if (link.id === args.id) {
+          links[index].description = args.description;
+          links[index].url = args.url;
+        }
+
+        return link.id === args.id;
+      });
+      return updatedLink[0];
+    },
+
+    deleteLink: (root, args) => {
+      const deletedLink = links.filter((link, index) => {
+        if (link.id === args.id) {
+          return links.splice(index, 1);
+        }
+      });
+      return deletedLink[0];
+    }
   }
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
 
