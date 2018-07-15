bde4f635f4ba0ba47d18deea96de2c98fe9d6ff2
wederribas <wederribas@gmail.com>
2018-07-15 10:17:22 -0300

    Refactor resolvers in modular files and update index.js

--- src/index.js
+++ src/index.js
@@ -1,48 +1,13 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { Prisma } = require("prisma-binding");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const AuthPayload = require("./resolvers/AuthPayload");
 
 const resolvers = {
-  Query: {
-    info: () => null,
-    feed: (root, args, context, info) => {
-      return context.db.query.links({}, info);
-    }
-  },
-
-  Mutation: {
-    post: (root, args, context, info) => {
-      return context.db.mutation.createLink(
-        {
-          data: {
-            url: args.url,
-            description: args.description
-          }
-        },
-        info
-      );
-    },
-
-    updateLink: (root, args) => {
-      const updatedLink = links.filter((link, index) => {
-        if (link.id === args.id) {
-          links[index].description = args.description;
-          links[index].url = args.url;
-        }
-
-        return link.id === args.id;
-      });
-      return updatedLink[0];
-    },
-
-    deleteLink: (root, args) => {
-      const deletedLink = links.filter((link, index) => {
-        if (link.id === args.id) {
-          return links.splice(index, 1);
-        }
-      });
-      return deletedLink[0];
-    }
-  }
+  Query,
+  Mutation,
+  AuthPayload
 };
 
 const server = new GraphQLServer({
@@ -53,6 +18,7 @@ const server = new GraphQLServer({
     db: new Prisma({
       typeDefs: "src/generated/prisma.graphql",
       endpoint: "https://us1.prisma.sh/weder-ribas-885196/hackernews-node/dev",
+      secret: "mysecret123",
       debug: true
     })
   })
